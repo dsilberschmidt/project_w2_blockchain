3067206dce69e04ddf75816f93ef1598546483b0
Daniel Silberschmidt <daniel.silberschmidt@gmail.com>
2021-07-25 10:25:17 +0200

    write file

--- models/Blockchain.js
+++ models/Blockchain.js
@@ -1,10 +1,21 @@
+const {BLOCKHAIN_FILE} = require('../config');
+const fs = require('fs');
+
+
 class Blockchain {
   constructor() {
+    //if file exist read from it else, create it
     this.blocks = [];
   }
 
   addBlock(block) {
     this.blocks.push(block);
+    //save to file
+
+    fs.writeFile(BLOCKHAIN_FILE, JSON.stringify(this.blocks), (err) => {
+      if (err) throw err;
+      console.log('The file has been saved!');
+      })
   }
 
   blockHeight() {
