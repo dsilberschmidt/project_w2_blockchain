58b576f81e5f0319a4c317fb3c2e836668f61c60
Daniel Silberschmidt <daniel.silberschmidt@gmail.com>
2021-07-25 11:06:41 +0200

    read blockchain file

--- models/Blockchain.js
+++ models/Blockchain.js
@@ -1,18 +1,29 @@
-const {BLOCKHAIN_FILE} = require('../config');
+const {BLOCKCHAIN_FILE} = require('../config');
 const fs = require('fs');
 
 
 class Blockchain {
   constructor() {
+    //let data = ''
     //if file exist read from it else, create it
-    this.blocks = [];
+    if (fs.existsSync(BLOCKCHAIN_FILE)) {
+        fs.readFile(BLOCKCHAIN_FILE, (err, data) => {
+          if (err) throw err;
+          //console.log(JSON.parse(data));
+          this.blocks = JSON.parse(data);
+          });
+        //this.blocks = JSON.parse(data);
+    }
+    else {
+      this.blocks = [];
+    }
   }
 
   addBlock(block) {
     this.blocks.push(block);
     //save to file
 
-    fs.writeFile(BLOCKHAIN_FILE, JSON.stringify(this.blocks), (err) => {
+    fs.writeFile(BLOCKCHAIN_FILE, JSON.stringify(this.blocks), (err) => {
       if (err) throw err;
       console.log('The file has been saved!');
       })
