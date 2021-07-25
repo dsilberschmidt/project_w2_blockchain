30ec47d1bd36c7c1e5db2944e2b6a044ae5871d4
Daniel Silberschmidt <daniel.silberschmidt@gmail.com>
2021-07-25 11:33:09 +0200

    bug detected. At the begining should generate utxo array from Blockchain, in case was read from file,  and not asume that is null.

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 const Blockchain = require('./models/Blockchain.js');
 
 const db = {
-  blockchain: new Blockchain,
-  utxos: [], 
+  blockchain: new Blockchain
+  // should read utxo from Blockchain
+  utxos: [],
 }
 
 module.exports = db;
